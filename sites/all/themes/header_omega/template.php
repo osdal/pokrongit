a4d928f3e573711a8dee56567e0e1678c8b0eff0
osdal <osdal67@gmail.com>
2017-08-16 17:29:45 +0300

    Алилуйя. Замена кнопки поиска

--- sites/all/themes/header_omega/template.php
+++ sites/all/themes/header_omega/template.php
@@ -10,3 +10,30 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+ 
+ /*Перекрываем форму поиска */
+function header_omega_form_alter(&$form, &$form_state, $form_id) {
+
+  if ($form_id == 'search_block_form') {
+
+    $deftext = t('Поиск');
+    $form['search_block_form']['#title'] = t(' '); 
+    $form['search_block_form']['#title_display'] = 'invisible'; 
+    $form['search_block_form']['#size'] = 24;  
+    $form['search_block_form']['#default_value'] = $deftext; 
+    $form['actions']['submit']['#value'] = '<div>'; // Change the text on the submit button
+	//$form['actions']['submit']['#value'] = t('Искать'); 
+
+
+    $form['search_block_form']['#attributes']['onblur'] = "if (this.value == '') {this.value = '".$deftext."';}";
+    $form['search_block_form']['#attributes']['onfocus'] = "if (this.value == '".$deftext."') {this.value = '';}";
+	
+	$form['actions']['submit'] = array(
+      '#type'   => 'markup',
+      '#prefix' => '<i class="fa fa-search" aria-hidden="true">',
+      '#suffix' => '</i>',
+      '#markup' => '<i class="icon-search icon-white"></i>');
+	
+	//$form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/search.png');
+  }
+}
